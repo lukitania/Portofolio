ea060cfc5cf083d461aec06003cd1c49828dc272
lukitania <0812tania@gmail.com>
2025-08-04 15:47:36 +0700

    progres navbar, about, and homepage

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,42 +1,54 @@
-import { useEffect } from "react";
+import { useEffect } from "react"
 
 export const Navbar = ({menuOpen, setMenuOpen}) => {
     useEffect(() => {
         document.body.style.overflow = menuOpen ? "hidden" : "";
     }, [menuOpen]);
-    return (
-        <nav className="fixed top-0 w-full z-40 bg-[rgba(10,10,10,0.8)] backdrop-blur-lg border-b border-white/10 shadow-lg">
-            <div className="max-w-5xl mx-auto px-4">
-                <div className="flex justify-between item-center h-16">
-                    <a herf="#home" className="font-mono text-xl font-bold text-white">
-                        {""}
-                        pdero<span className="text-blue-500">.tech</span>{""}
+    return(
+    <nav className="fixed top-0 w-full z-40 bg-[rgba(10.10,10,0.8)] backdrop-blur-lg border-b border-white/10 shadow-lg">
+        <div className="max-w-5xl mx-auto px-4">
+            <div className="flex justify-between items-center h-16">
+                <a hre f="#home" className="font-mono text-xl font-bold text-white"> 
+                    {""}
+                    Tania<span className="text-blue-500">.tech</span>{""}
+                </a>
+                <div className="w-7 h-5 relative cursor-pointer z-40 md:hidden" 
+                onClick={() =>setMenuOpen((prev) => !prev)}>
+                    &#9776;
+                </div>
+
+                <div className="hidden md:flex item-center space-x-8">
+                    <a 
+                        href="#home" 
+                        className="text-gray-300 hove:text-white transition-colors"
+                    >
+                        {" "}
+                        Home{" "}
                     </a>
-
-                    <div className="w-7 h-5 relative cursor-pointer z-40 md:hidden" onClick={() => setMenuOpen((prev) => !prev)}>
-                        &#9776;
-                    </div>
-
-                    <div className="hidden md:flex items-center space-x-8">
-                        <a herf="#home" className="tex-gray-300 hove:text-white transition-colors">
-                            {""}
-                            Home{""}
-                        </a>
-                        <a herf="#about" className="tex-gray-300 hove:text-white transition-colors">
-                            {""}
-                            About{""}
-                        </a>
-                        <a herf="#projects" className="tex-gray-300 hove:text-white transition-colors">
-                            {""}
-                            Projects{""}
-                        </a>
-                        <a herf="#contact" className="tex-gray-300 hove:text-white transition-colors">
-                            {""}
-                            Contact{""}
-                        </a>
-                    </div>
+                    <a 
+                        href="#about" 
+                        className="text-gray-300 hove:text-white transition-colors"
+                    >
+                        {" "}
+                        About{" "}
+                    </a>
+                    <a 
+                        href="#projects" 
+                        className="text-gray-300 hove:text-white transition-colors"
+                    >
+                        {" "}
+                        Projects{" "}
+                    </a>
+                    <a 
+                        href="#contact" 
+                        className="text-gray-300 hove:text-white transition-colors"
+                    >
+                        {" "}
+                        Contact{" "}
+                    </a>
                 </div>
             </div>
-        </nav>
-    );
-};
+        </div>
+    </nav>
+    )
+}
