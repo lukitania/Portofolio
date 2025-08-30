b4e58e5bf07ed5cbdf93f8995367b1a7fdb1d9dd
lukitania <0812tania@gmail.com>
2025-08-30 22:55:29 +0700

    modified about

--- src/components/sections/About.jsx
+++ src/components/sections/About.jsx
@@ -255,6 +255,16 @@ export const About = () => {
 
 
                     <div className="grid grid-cols-2 gap-4">
+                        <div className="w-full max-w-md mx-auto">
+                            <img
+                            src="/assets/teldu4.jpg"
+                            alt="startup template"
+                            width={500}
+                            height={500}
+                            className="h-20 w-full rounded-lg object-cover shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset] md:h-44 lg:h-29"
+                        />
+                        </div>
+
                         <img
                             src="/assets/Teldu1.jpg"
                             alt="startup template"
@@ -271,39 +281,13 @@ export const About = () => {
                         />
                         <div className="w-full max-w-md mx-auto">
                             <img
-                            src="/assets/msib1.jpg"
+                            src="/assets/teldu3-jpg.jpg"
                             alt="startup template"
                             width={500}
                             height={500}
                             className="h-20 w-full rounded-lg object-cover shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset] md:h-44 lg:h-29"
                         />
                         </div>
-
-
-                        <div className="w-full max-w-md mx-auto">
-                            <img
-                            src="/assets/msib2.png"
-                            alt="startup template"
-                            width={500}
-                            height={500}
-                            className="h-20 w-full rounded-lg object-cover shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset] md:h-44 lg:h-29"
-                        />
-                        </div>
-
-                        <img
-                            src="/assets/msib3.png"
-                            alt="startup template"
-                            width={500}
-                            height={500}
-                            className="h-20 w-full rounded-lg object-cover shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset] md:h-44 lg:h-29"
-                        />
-                        <img
-                            src="/assets/Onboarding Bisa Ai 2024-02-16 (2).png"
-                            alt="startup template"
-                            width={500}
-                            height={500}
-                            className="h-20 w-full rounded-lg object-cover shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset] md:h-44 lg:h-29"
-                        />
 
                     </div>
                 </div>
